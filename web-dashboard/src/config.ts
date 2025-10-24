a47b9d5fb556a8f1174f9106921888eb0bbbdfb9
Aravinth V <codeforgebyaravinth@gmail.com>
2025-10-24 14:33:14 +0530

    Create config.ts

---
+++ web-dashboard/src/config.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = 'https://byyfvcfxyomwojpfiazw.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5eWZ2Y2Z4eW9td29qcGZpYXp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyODgwNzQsImV4cCI6MjA3Njg2NDA3NH0.HNN14TCZ__wt-dQfS1G8ULAeWQVpdy_qAROx7YSHqI4';
+export const CLOUDFLARE_WORKER_URL = 'https://college-backend.codeforgebyaravinth.workers.dev/';
